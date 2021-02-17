527caa77fa2a33e7a4cded37073630b06ff3761e
Bertrand <bertrand.bevillard@gmail.com>
2021-02-17 18:02:50 +0100

    fixed import

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
-const {eig} = require('eigen');
-// import eig from 'eigen'
+const eig = require('eigen');
 
 let win, M;
 function createWindow() {
@@ -30,22 +29,15 @@ app.on('activate', () => {
 })
 
 ipcMain.on('generateMatrix', async (event) => {
-  console.log('Here on generateMatrix');
-
-  
   await eig.ready
   M = new eig.Matrix.random(10,10);
   M.print("M")
-  // win.webContents.send('resultMatrix1', M.print("M"));
+  eig.GC.flush();
 });
 
 ipcMain.on('calcMatrix', async (event) => {
-  console.log('Here on calcMatrix');
-
-
   await eig.ready
-  M.inverse();
-  M.print("M")
-  // win.webContents.send('resultMatrix2', M.print("M"));
+  M = new eig.Matrix.random(10,10);
+  M.print("M");
   eig.GC.flush();
 });
